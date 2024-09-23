3022ff77cb049ff68eaf68526ac453087c41147f
PiaLember <pia.lember@gmail.com>
2024-09-23 09:33:58 +0300

    Spaceship setup

---
+++ Shop/Shop.Data/ShopContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Core.Domain;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Xml;
+
+namespace Shop.Data
+{
+    public class ShopContext : DbContext
+
+    {
+        public ShopContext(DbContextOptions<ShopContext> options)
+        : base(options) { }
+
+        public DbSet<Spaceship> Spaceships { get; set; }
+    }
+}
