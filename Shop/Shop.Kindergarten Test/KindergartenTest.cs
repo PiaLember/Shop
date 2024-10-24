433e064e13f06bad957209685662463883318fd9
PiaLember <pia.lember@gmail.com>
2024-10-24 11:08:38 +0300

    Add DeleteById test

--- Shop/Shop.Kindergarten Test/KindergartenTest.cs
+++ Shop/Shop.Kindergarten Test/KindergartenTest.cs
@@ -1,11 +1,31 @@
+using Shop.Core.Dto;
+using Shop.Core.ServiceInterface;
+
 namespace Shop.Kindergarten_Test
 {
-    public class KindergartenTest
+    public class KindergartenTest : TestBase
     {
         [Fact]
-        public void Test1()
+        public async Task Should_DeleteByIdKindergarten_WhenDeleteKindergarten()
         {
+            KindergartenDto kindergarten = MockkindergartenData();
+            var addData = await Svc<IKindergartensServices>().Create(kindergarten);
+            var result = await Svc<IKindergartensServices>().Delete((Guid)addData.Id);
+            Assert.Equal(result, addData);
+        }
 
+        private KindergartenDto MockkindergartenData()
+        {
+            KindergartenDto kindergarten = new()
+            {
+                GroupName = "Test",
+                ChildrenCount = 1,
+                KindergartenName = "Test",
+                Teacher = "Test",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+            return kindergarten;
         }
     }
 }
