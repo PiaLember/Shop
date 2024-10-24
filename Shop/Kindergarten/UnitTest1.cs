ac2c2000c26a2a6df283004516060c654f49d507
PiaLember <pia.lember@gmail.com>
2024-10-24 10:54:23 +0300

    KindergartenTest initial commit

---
+++ Shop/Kindergarten/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Kindergarten
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Shop/Shop.Kindergarten Test/KindergartenTest.cs
@@ -0,0 +1,11 @@
+namespace Shop.Kindergarten_Test
+{
+    public class KindergartenTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
