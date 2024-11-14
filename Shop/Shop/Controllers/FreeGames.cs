47294b24f432d2705453f31ecf2678eb505fbaf8
PiaLember <pia.lember@gmail.com>
2024-11-14 13:21:08 +0200

    Add sort order

--- Shop/Shop/Controllers/FreeGames.cs
+++ Shop/Shop/Controllers/FreeGames.cs
@@ -16,10 +16,11 @@ namespace Shop.Controllers
         {
             _freeGamesServices = freeGamesServices;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
             FreeGamesResultDto dto = new();
-            _freeGamesServices.FreeGamesResult(dto);
+            await _freeGamesServices.FreeGamesResult(dto);
+
             var vmList = dto.FreeGames.Select(game => new IndexViewModel
             {
                 id = game.id,
@@ -33,10 +34,23 @@ namespace Shop.Controllers
                 developer = game.developer,
                 release_date = game.release_date,
                 freetogame_profile_url = game.freetogame_profile_url
-            }).ToList();
+            });
+            vmList = sortOrder switch
+            {
+                "name_desc" => vmList.OrderByDescending(g => g.title),
+                "name_asc" => vmList.OrderBy(g => g.title),
+                "genre_desc" => vmList.OrderByDescending(g => g.genre),
+                "genre_asc" => vmList.OrderBy(g => g.genre),
+                "platform_desc" => vmList.OrderByDescending(g => g.platform),
+                "platform_asc" => vmList.OrderBy(g => g.platform),
+                "release_date_desc" => vmList.OrderByDescending(g => g.release_date),
+                "release_date_asc" => vmList.OrderBy(g => g.release_date),
+                _ => vmList.OrderBy(g => g.title)  // Default to name ascending
+            };
 
-            return View(vmList);
+            return View(vmList.ToList());
         }
+
 
         [HttpPost]
         public IActionResult ShowFreeGames(IndexViewModel model)
