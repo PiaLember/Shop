2515271b13d09633b20d8fd1641b568e98d29731
PiaLember <pia.lember@gmail.com>
2024-11-19 16:45:22 +0200

    Initial commit to OpenWeather

---
+++ Shop/Shop/Controllers/OpenWeathersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class OpenWeathersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
